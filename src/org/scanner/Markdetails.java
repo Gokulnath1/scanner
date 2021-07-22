81dca6a1addc2451b767ca6838db8be9c2c120d5
Gokulnath1 <gokulnathrm99@gmail.com>
2021-07-22 11:01:14 +0530

    print rm

--- src/org/scanner/Markdetails.java
+++ src/org/scanner/Markdetails.java
@@ -6,7 +6,7 @@ public class Markdetails {
 	   
 	    public static void main(String[] args) {
 			
-	    	System.out.println("gk");
+	    	System.out.println("rm");
 	    	Scanner s = new Scanner(System.in);
 	    	
 	    	System.out.println("enter the student id");
